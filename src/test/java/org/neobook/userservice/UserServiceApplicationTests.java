d6519606b45f8d66f28f78255e131cb212ca46f4
Todor Mitev <td_mitev@abv.bg>
2025-04-25 17:13:11 +0300

    Initial commit

---
+++ src/test/java/org/neobook/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neobook.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
